de6af2d3bb214ce0feb6718421c873359137e31e
Ryan Carroll <ryan@zylo.com>
2018-04-13 10:00:40 -0400

    mocha setup, initial basic test

--- api/services/chargeService.js
+++ api/services/chargeService.js
@@ -1,10 +1,17 @@
 'use strict';
 
+const _ = require('lodash');
+
 function createCharge() {
-  // const chargeObj = {
-  //
-  //
-  // }
+  const chargeObj = {
+    amount: _.sample([100, 250, 500, 750, 1000]),
+    date: '1/1/2018',
+    name: 'Salesforce CRM',
+    description: 'Annual renewal for company licenses',
+    type: 'AP'
+  };
+
+  return chargeObj;
 }
 
 function build(db, cb) {
