74408742f8799350f1643d81cac5cc50dc666fef
Ryan Carroll <ryan@zylo.com>
2018-04-13 09:46:10 -0400

    setup chargeService test

--- api/services/chargeService.js
+++ api/services/chargeService.js
@@ -1,9 +1,17 @@
 'use strict';
 
+function createCharge() {
+  // const chargeObj = {
+  //
+  //
+  // }
+}
+
 function build(db, cb) {
   cb(null, 'success');
 }
 
 module.exports = {
-  build: build
+  build: build,
+  createCharge: createCharge
 };
