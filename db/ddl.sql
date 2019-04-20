2a6a11b57089191e2ca7821e21da03aa0e2baae8
Brandon Johnson <brandon.johnson@salesforce.com>
2019-04-20 15:45:49 -0400

    Added DB init script to load ddl.

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
-CREATE TABLE projects (
+CREATE TABLE IF NOT EXISTS projects (
   id uuid DEFAULT gen_random_uuid() NOT NULL,
   name text NOT NULL,
   created_at timestamp with time zone DEFAULT now()
@@ -9,3 +9,12 @@ CREATE TABLE projects (
 
 INSERT INTO projects(name)
 SELECT 'chance';
+
+CREATE TABLE IF NOT EXISTS charges (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    amount numeric NOT NULL,
+    name varchar(128) NOT NULL,
+    description varchar(250),
+    type varchar(64),
+    created_at timestamp with time zone DEFAULT now()
+);
